4b99613f3367c9fc6fc0af24a622ae85c0e77bd0
daeukii <gjeodnr2@gmail.com>
2024-02-05 16:38:54 +0900

    240205

--- src/main/java/com/example/jpanext/shop/ChildService.java
+++ src/main/java/com/example/jpanext/shop/ChildService.java
@@ -1,3 +1,5 @@
+package com.example.jpanext.shop;
+
 import com.example.jpanext.shop.entity.Customer;
 import com.example.jpanext.shop.repo.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChildService {
     private final CustomerRepository customerRepository;
 
+    // 나를 호출한 메서드가 트랜잭션 이면 그 일부로 실행되고,
+    // 아니라면 트랜잭션 없이 실행된다.
     @Transactional(propagation = Propagation.SUPPORTS)
     public void supports() {
         customerRepository.save(Customer.builder()
@@ -17,10 +21,19 @@ public class ChildService {
         throw new RuntimeException("child throw");
     }
 
-    // 나를 호출한 메서드가
-
+    // 나를 호출한 메서드가 트랜잭션 이어야 한다.
+    // 없으면 예외가 발생한다.
     @Transactional(propagation = Propagation.MANDATORY)
     public void mandatory() {
+        customerRepository.save(Customer.builder()
+                .name("Child Mandatory").build());
+        throw new RuntimeException("child throw");
+    }
+
+    // 나를 호출한 메서드가 트랜잭션이 아니어야 한다.
+    // 있으면 예외가 발생한다.
+    @Transactional(propagation = Propagation.NEVER)
+    public void never() {
         customerRepository.save(Customer.builder()
                 .name("Child Mandatory").build());
         throw new RuntimeException("child throw");
--- src/main/java/com/example/jpanext/shop/ShopController.java
+++ src/main/java/com/example/jpanext/shop/ShopController.java
@@ -10,10 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ShopController {
     private final ShopService shopService;
+    private final ParentService parentService;
+    private final EntityManagerService emService;
 
     @GetMapping("create-order")
     public String createOrder() {
         shopService.createOrder();
         return "done";
+    }
+
+    @GetMapping("propagation")
+    public void propagation() {
+        parentService.none();
+    }
+
+    @GetMapping("identity")
+    public void identity() {
+        shopService.testIdentity();
+    }
+
+    @GetMapping("dirty-check")
+    public void dirtyCheck() {
+        shopService.testDirtyChecking();
+    }
+
+    @GetMapping("test-em")
+    public void testEm() {
+        emService.save();
+        emService.find();
     }
 }
